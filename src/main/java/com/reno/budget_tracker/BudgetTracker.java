784c143d23ef3c2c20668e9f36153e4d2e2972be
Chris <98862485+Britten66@users.noreply.github.com>
2026-05-28 01:49:23 -0300

    budgetTracker methods complete

--- src/main/java/com/reno/budget_tracker/BudgetTracker.java
+++ src/main/java/com/reno/budget_tracker/BudgetTracker.java
@@ -14,14 +14,22 @@ import java.util.List;
  * ------
  * this will contain more random notes and thinking along the way
  *
+ *setting total to 0 ahead of time when building out that method
+ *an important part of clean code chp 2, using meaningful names will be added on to this starting with the for the loop
  *
+ *the for loop checks for singleExpense sum & returns the total which was set to 0 before.
+ *while the loop is running it checks for each singleExpense inside expenses
+ *
+ *getRemaining budget takes budget and takes away whatever getTotalSpent value is
+ *
+ * getSpentByCategory loops and will ONLY add an amound based on cateory needing to match & this also uses ignorecase.
  */
 
 public class BudgetTracker {
 
     private final double budget;
 
-    private final list<Expenses> expenses = new ArrayList<>();
+    private final List<Expenses> expenses = new ArrayList<>();
 
     public BudgetTracker(double budget) {
         this.budget = budget;
@@ -31,4 +39,46 @@ public class BudgetTracker {
         expenses.add(expense);
     }
 
+    public double getTotalExpense() {
+        double total = 0;
+
+        for (Expenses singleExpense : expenses) total += singleExpense.getAmount();
+        return total;
+
+    }
+
+
+    public double getRemainingBudget() {
+        return budget - getTotalExpense();
+    }
+
+
+    public double getSpentByCategory(String category) {
+
+        double total = 0;
+        for (Expenses singleExpense : expenses) {
+            if (singleExpense.getCategory().equalsIgnoreCase(category))
+
+                total += singleExpense.getAmount();
+        }
+
+        return total;
+    }
+
+    public boolean isOverBudget() {
+        return getTotalExpense() > budget;
+
+    }
+
+    public double getPercentUsed() {
+        if (budget == 0)
+            return 0;
+
+
+        return (getTotalExpense() / budget) * 100;
+    }
+
 }
+
+
+
