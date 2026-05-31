a2eda6096a27037b7370b6f9d80bcd64e341d89e
Chris <98862485+Britten66@users.noreply.github.com>
2026-05-30 22:57:36 -0300

    added majority of tests, still finishing up some and adding comments

--- src/test/java/com/reno/budget_tracker/BudgetTrackerTests.java
+++ src/test/java/com/reno/budget_tracker/BudgetTrackerTests.java
@@ -11,7 +11,17 @@ package com.reno.budget_tracker;
  * using beforeEach will run ahead of time, helps to not repeat myself or re enter an amount while testing
  * setUp will be void as it wont be returning anything
  *
- *adding test for
+ *
+ * here are all tests for what could fail
+ *
+ *adding test for adding expenses and then checking if it worked correctly
+ * that will be test 1
+ *
+ *test 2 will be making sure subtraction works
+ *
+ *test 3 will check total spent by each category
+ *
+ *test 4 is for matching and confirming it will work wit hcase sensitivity
  *
  *
  *
@@ -27,24 +37,67 @@ public class BudgetTrackerTests {
     private BudgetTracker tracker;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
 
-    tracker = new BudgetTracker(13500);
+        tracker = new BudgetTracker(13500);
+    }
+
+
+    @Test
+    void totalExpensesAddedUp() {
+        tracker.addExpense(new Expenses("paint", 400));
+        tracker.addExpense(new Expenses("flooring", 2200));
+
+        assertEquals(2600, tracker.getTotalExpense());
+
+
+    }
+
+
+    @Test
+    void remainingBudgetIsCorrect() {
+        tracker.addExpense(new Expenses("paint", 200));
+        assertEquals(13300, tracker.getRemainingBudget());
+    }
+
+    @Test
+    void spentByCategory() {
+
+        tracker.addExpense(new Expenses("paint", 100));
+         tracker.addExpense(new Expenses("paint", 50));
+           assertEquals(150, tracker.getSpentByCategory("paint"));
+
+
+    }
+
+
+    @Test
+    void caseSensitiveMatching() {
+        tracker.addExpense(new Expenses("Paint", 100));
+        assertEquals(100, tracker.getSpentByCategory("paint"));
+
+
+    }
+
+
+    @Test
+    void categoryUnknownReturn(){
+        tracker.addExpense(new Expenses("paint", 100));
+        assertEquals(0, tracker.getSpentByCategory("flooring"));
+    }
+
+
+    @Test
+    void emptyTracker(){
+        assertEquals(0, tracker.getTotalExpense());
+    }
+
+
+
+    @Test
+    void
+
+
 }
 
 
-
-
-
-
-@Test
-void totalExpensesAddedUp(){
-    tracker.addExpense(new Expenses("paint", 400));
-    tracker.addExpense(new Expenses("flooring", 2200));
-
-assertEquals(2600, tracker.getTotalExpense());
-}
-
-
-
-}
